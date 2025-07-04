049d948aaaeed9de3dd52e5cc11868d02d411c5b
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-07-04 15:52:40 -0400

    statuscol.lua: update

--- debian/statuscol.lua
+++ debian/statuscol.lua
@@ -1,11 +1,11 @@
 local builtin = require("statuscol.builtin")
 require("statuscol").setup({
     relculright = true,
-    bt_ignore= { "help" },
-    ft_ignore= { "man"  },
+    bt_ignore= {"help"}, ft_ignore= {"man"},
     segments = {
-        { sign = { namespace = { "diagnostic/signs" }, colwidth = 2 }, click = "v:lua.ScSa" },
+        { sign = { namespace = {".*diagnostic.signs"} }, click = "v:lua.ScSa" },
         { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-        { sign = { namespace = { "gitsigns" }, colwidth = 1, wrap = true, fillchar = "│", fillcharhl = "LineNr" }, click = "v:lua.ScSa" },
+        { sign = { namespace = {"gitsigns"}, colwidth = 1, wrap = true, fillchar = "│", fillcharhl = "LineNr" }, click = "v:lua.ScSa" },
+        { text = { " " } }
     }
 })
