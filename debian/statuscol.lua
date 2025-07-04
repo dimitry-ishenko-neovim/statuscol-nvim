f4e96b2a011552d3bfed879ba75eff5ab1060938
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-07-04 01:56:08 -0400

    statuscol.vim => statuscol.lua

---
+++ debian/statuscol.lua
@@ -0,0 +1,11 @@
+local builtin = require("statuscol.builtin")
+require("statuscol").setup({
+    relculright = true,
+    bt_ignore= { "help" },
+    ft_ignore= { "man"  },
+    segments = {
+        { sign = { namespace = { "diagnostic/signs" }, colwidth = 2 }, click = "v:lua.ScSa" },
+        { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+        { sign = { namespace = { "gitsigns" }, colwidth = 1, wrap = true, fillchar = "│", fillcharhl = "LineNr" }, click = "v:lua.ScSa" },
+    }
+})
