cd6ced918dee772cc5b5eaa5ab738402fa6d2070
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-07-07 14:27:18 -0400

    statuscol.lua: ignore oil windows

--- debian/statuscol.lua
+++ debian/statuscol.lua
@@ -1,7 +1,7 @@
 local builtin = require("statuscol.builtin")
 require("statuscol").setup({
     relculright = true,
-    bt_ignore= {"help"}, ft_ignore= {"man"},
+    bt_ignore= {"help"}, ft_ignore= {"man", "oil"},
     segments = {
         { sign = { name = {".*"}, text = {".*"} }, click = "v:lua.ScSa" },
         { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
