f753dc1dbff0b9d0649ef08f31ad971cae5fa7b4
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-07-07 14:26:50 -0400

    statuscol.lua: show all signs in the first column

--- debian/statuscol.lua
+++ debian/statuscol.lua
@@ -3,7 +3,7 @@ require("statuscol").setup({
     relculright = true,
     bt_ignore= {"help"}, ft_ignore= {"man"},
     segments = {
-        { sign = { namespace = {".*diagnostic.signs"} }, click = "v:lua.ScSa" },
+        { sign = { name = {".*"}, text = {".*"} }, click = "v:lua.ScSa" },
         { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
         { sign = { namespace = {"gitsigns"}, colwidth = 1, wrap = true, fillchar = "│", fillcharhl = "LineNr" }, click = "v:lua.ScSa" },
     }
